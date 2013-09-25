9089aa22c162726f23eaf3d21568c48df6d4e55f
Katrina Owen <katrina.owen@gmail.com>
2013-09-25 07:27:30 -0600

    Add github usernames to gschool1 class

--- lib/gschool/one.rb
+++ lib/gschool/one.rb
@@ -2,6 +2,36 @@ module GSchool
   class One
     extend Group
 
+    def self.github
+      [
+        "adam89",
+        "Agsiegert",
+        "benhorne44",
+        "billy22g",
+        "breethomas",
+        "BryanaKnight",
+        "bwinterling",
+        "dpequeen",
+        "edatrix",
+        "Egogre",
+        "fluxusfrequency",
+        "JonahMoses",
+        "LouisaBarrett",
+        "LukeMartinez",
+        "mappingkat",
+        "nikhiltaneja",
+        "pzula",
+        "QuillyT",
+        "rolentle",
+        "rrgayhart",
+        "srt32",
+        "thewatts",
+        "TylerLong86",
+        "wvmitchell",
+        "x46672"
+      ]
+    end
+
     def self.all
       [
         "Adam Magan",
