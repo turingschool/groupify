1edae48fc33e7725b01fd2f955baefbf09eb688e
Katrina Owen <katrina.owen@gmail.com>
2013-11-04 15:03:34 -0700

    Complicate the cohort fixture

--- test/gschool/cohort_test.rb
+++ test/gschool/cohort_test.rb
@@ -10,15 +10,15 @@ module GSchool
   class CohortTest < Minitest::Test
     def test_simple_cohort
       cohort = Cohort.new(%w(Charlie Alice Bob), %w(alice charlie Bob))
-      assert_equal [:"Alice", :"Bob", :"Charlie"], cohort.students
+      assert_equal [:Alice, :Bob, :Charlie], cohort.students
       assert_equal cohort.students, cohort.all
       assert_equal [:alice, :Bob, :charlie], cohort.usernames
     end
 
     def test_cohort_after_student_left
       cohort = Cohort.new(%w(Charlie Alice Bob), %w(alice charlie Bob), %w(Bob))
-      assert_equal [:"Alice", :"Bob", :"Charlie"], cohort.students
-      assert_equal [:"Alice", :"Charlie"], cohort.all
+      assert_equal [:Alice, :Bob, :Charlie], cohort.students
+      assert_equal [:Alice, :Charlie], cohort.all
     end
 
     def test_handles_nil_input
@@ -52,10 +52,10 @@ module GSchool
       cohort.projects << Project.new(data)
 
       expected = [
-        [:"Alice", :"Bob"],
-        [:"Alice", :"Dave"],
-        [:"Bob", :"Charlie"],
-        [:"Charlie", :"Dave"]
+        [:Alice, :Bob],
+        [:Alice, :Dave],
+        [:Bob, :Charlie],
+        [:Charlie, :Dave]
       ]
       assert_equal expected.sort, cohort.teams.sort
     end
@@ -90,22 +90,22 @@ module GSchool
       cohort.projects << Project.new(data)
 
       expected = [
-        [:"Alice", :"Bob"],
-        [:"Alice", :"Dave"],
-        [:"Bob", :"Charlie"],
-        [:"Charlie", :"Dave"]
+        [:Alice, :Bob],
+        [:Alice, :Dave],
+        [:Bob, :Charlie],
+        [:Charlie, :Dave]
       ]
 
-      assert_equal [:"Bob", :"Dave"], cohort.teammates("Alice")
+      assert_equal [:Bob, :Dave], cohort.teammates("Alice")
     end
 
     def test_cohort_from_yml
       cohort = Cohort.from_yml('./test/fixtures/students.yml')
-      assert_equal [:"Alice", :"Bob", :"Charlie", :"Dave", :"Eve"], cohort.students
-      assert_equal [:"alice", :"bob", :"charlie", :"dave", :"eve"], cohort.usernames
-      assert_equal [:"Alice", :"Charlie", :"Dave", :"Eve"], cohort.all
+      assert_equal [:Alice, :Bob, :Charlie, :Dave, :Eve, :Fred], cohort.students
+      assert_equal [:alice, :bob, :charlie, :dave, :eve, :fred], cohort.usernames
+      assert_equal [:Alice, :Charlie, :Dave, :Eve, :Fred], cohort.all
 
-      assert_equal [:"Bob", :"Charlie", :"Eve"], cohort.teammates("Alice")
+      assert_equal [:Bob, :Charlie, :Eve, :Fred], cohort.teammates("Alice")
     end
   end
 end
